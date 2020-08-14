9b6666eaa8be8e0343bcabe6c90bad685cd00cb4
Mohammed849 <65894073+Mohammed849@users.noreply.github.com>
2020-08-14 08:05:49 +0200

    ReactNative

--- App.js
+++ App.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { ScrollView, View, Text, StyleSheet, Image } from 'react-native';
 class App extends React.Component {
   render() {
     return (
-      <View style={styles.Bgcgrey}>
+      <ScrollView style={styles.Bgcgrey}>
         <View>
           <Text style={styles.Taille}> Hello Mohammed</Text>
           <Text style={styles.centre}>How are you doing</Text>
           <Text style={styles.Engras}>Well done</Text>
+          <Image
+            style={styles.imgkonexio1}
+            source={require('./assets/konexio-logo_1.png')}
+          />
+          <Image
+            style={styles.imgkonexio1}
+            source={{
+              uri:
+                'https://www.konexio.eu/uploads/1/2/0/2/120245745/konexio-logo_1.png',
+            }}
+          />
         </View>
-      </View>
+      </ScrollView>
     );
   }
 }
@@ -29,6 +40,14 @@ const styles = StyleSheet.create({
   },
   Engras: {
     fontWeight: 'bold',
+  },
+  imgkonexio1: {
+    width: 100,
+    height: 100,
+  },
+  imgkonexio2: {
+    width: 100,
+    height: 100,
   },
 });
 
